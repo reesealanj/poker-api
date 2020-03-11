4f2e773f1724ed382ccc8b29ea9879584d5746cd
Reese Jones <reesealanj@gmail.com>
2020-03-11 01:31:21 -0400

    Added basic client index page

---
+++ client/index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>PHP Application</title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>This is a potential front end.</h1>";
+        ?>
+    </body>
+</html>
